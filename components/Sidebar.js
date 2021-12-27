0dcb33b06592e2cb1e87184c2d2cb57fbd1a05be
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-12-27 20:01:32 +0530

    aadded features

---
+++ components/Sidebar.js
@@ -0,0 +1,16 @@
+import {
+    HomeIcon
+} from "@heroicons/react/outline";
+function Sidebar() {
+    return (
+        <div>
+            <div>
+                <button>
+                    {/* <HomeIcon/> */}
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
